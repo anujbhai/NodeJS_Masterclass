47b95341586db475114b6b45fb01a5105d744fd6
ANUJ UPADHYAY <openmindedlearning@gmail.com>
2018-12-18 15:20:09 +0530

    Add files via upload

---
+++ app/config.js
@@ -0,0 +1,29 @@
+/*
+*
+* Create and export configuration variables
+*
+*/
+
+// Container for all the environments
+var environments = {};
+
+// Staging (default) environment
+environments.staging = {
+	'port': 3000,
+	'envName': 'staging'
+};
+
+// Production environment
+environments.production = {
+	'port': 5000,
+	'envName': 'production'
+};
+
+// Determine which environment was passed as a command line arguement
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check the current environment, if the defined environment is not found, then default to staging
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+//  Export the module
+module.exports = environmentToExport;
